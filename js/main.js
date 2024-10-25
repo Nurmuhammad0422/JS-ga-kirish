7cb224d8ad482cb88f8bafa865604a723a441be4
Nurmuhammad0422 <bnurmuhammad0422@gmail.com>
2024-10-25 19:48:33 +0500

    Js form end

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var question1 = "HTML document structure qanday tuziladi?"
+var question2 = "HTMLda <header>, <footer>, <main> kabi semantik teglardan nima uchun foydalaniladi?"
+var question3 = "Formalarda input turlari qanday va ularning har biri qanday ishlatiladi?"
+var question4 = "CSS'da class va id selektorlarining o'xshashliklari va farqlari qanday?"
+var question5 = "SCSS mixin'lari qanday yoziladi va qachon ishlatish foydali?"
+var question6 = "Tailwind ossonroqmi yoki scss?"
+var question7 = "Rasmni HTMLga qanday qoyiladi?"
+var question8 = "CSS'da hover kabi efektlar qanday qilinadi?"
+var question9 = "SCSS'da fayllarni qanday qilib bo'laklarga ajratamiz?"
+var question10 = "Formadagi input turi qanday ma'nolarni anglatadi?"
+var question11 = "SEO uchun HTML qanday yordam beradi?"
+var question12 = "Ramni Htmlga qoygandan song nima bilan uni bossa boshqa pagega otadigan qilsa boladi?"
+var question13 = "Input qoygandan so'ng nima atribute qoysa yozib bo'lmaydgan qilsa boladi?"
+var question14 = "HTMLda alt atributi nima va u qayerda ishlatiladi?"
+var question15 = "Margin va padding o'rtasidagi farq nimada?"
+var question16 = "CSS'da display: none nima vazifani bajaradi?"
+var question17 = "CSS transform funksiyasi nima va qanday ishlaydi?"
+var question18 = "Tailwindda rang va shriftdagi o'zgarishlarni qanday qilish mumkin?"
+var question19 = "Tailwindda line-heigh nima bilan beriladi?"
+var question20 = "CSSda media yoziladimi va u nimani bajaradi?"
+
+var answer1 = prompt (question1)
+var answer2 = prompt (question2)
+var answer3 = prompt (question3)
+var answer4 = prompt (question4)
+var answer5 = prompt (question5)
+var answer6 = prompt (question6)
+var answer7 = prompt (question7)
+var answer8 = prompt (question8)
+var answer9 = prompt (question9)
+var answer10 = prompt (question10)
+var answer11 = prompt (question11)
+var answer12 = prompt (question12)
+var answer13 = prompt (question13)
+var answer14 = prompt (question14)
+var answer15 = prompt (question15)
+var answer16 = prompt (question16)
+var answer17 = prompt (question17)
+var answer18 = prompt (question18)
+var answer19 = prompt (question19)
+var answer20 = prompt (question20)
+
+console.log(question1,answer1)
+console.log(question2,answer2)
+console.log(question3,answer3)
+console.log(question4,answer4)
+console.log(question5,answer5)
+console.log(question6,answer6)
+console.log(question7,answer7)
+console.log(question8,answer8)
+console.log(question9,answer9)
+console.log(question10,answer10)
+console.log(question11,answer11)
+console.log(question12,answer12)
+console.log(question13,answer13)
+console.log(question14,answer14)
+console.log(question15,answer15)
+console.log(question16,answer16)
+console.log(question17,answer17)
+console.log(question18,answer18)
+console.log(question19,answer19)
+console.log(question20,answer20)
